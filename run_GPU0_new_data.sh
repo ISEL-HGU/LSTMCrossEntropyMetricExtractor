7cb9e938fd3ffb5f8d5d492a5f1791c2b54c91f1
Eunji Won <21500420@handong.edu>
2020-04-19 20:37:02 +0900

    add new data script

---
+++ run_GPU0_new_data.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+for projectname in "bval" "incubator-hivemall";
+do
+python main_GPU0.py --batch_size 32 --test_batch_size 16 --epochs 20 --lr 0.01 --seq_size 32 --embedding_size 64 --lstm_size 64 --gradients_norm 5 --input_csv_metric_file /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/Output/LM/${projectname}-reference/${projectname}_developer.csv --output_csv_metric_file ./data/test/${projectname}_Add_LSTM_metric.csv --train_file /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/TrainData/${projectname}_AllCommitsAddedLines.txt --test_file /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/TrainData/Commit/${projectname}/
+done
+
+
+
+
+
+
