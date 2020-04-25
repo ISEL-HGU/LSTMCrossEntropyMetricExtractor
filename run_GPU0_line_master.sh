2fe8880e22a02abefde59385515a1eaf233967aa
Eunji Won <21500420@handong.edu>
2020-04-26 08:32:42 +0900

    change master LSTM

--- run_GPU0_line_master.sh
+++ run_GPU0_line_master.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 # Master LSTM
-for projectname in "ace" "ant-ivy" "bigtop" "bval" "camel" "cayenne" "cordova-android" "creadur-rat" "crunch" "deltaspike" "gora" "groovy" "guacamole-client" "incubator-dolphinscheduler" "incubator-hivemall";
-do
-python main_line_GPU0.py --batch_size 32 --test_batch_size 32 --epochs 20 --lr 0.01 --seq_size 32 --embedding_size 64 --lstm_size 64 --gradients_norm 5 --input_csv_metric_file /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/Output/DP/label_DP/${projectname}_developer.csv --output_csv_metric_file ./data/test/${projectname}_Line_Master_LSTM_Metric.csv --train_file /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/TrainData/Master_AllCommitsAddedLines.txt --test_file /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/TrainData/Commit/${projectname}/
-done
-
+python main_line_GPU0_master.py --batch_size 32 --test_batch_size 32 --epochs 20 --lr 0.01 --seq_size 32 --embedding_size 64 --lstm_size 64 --gradients_norm 5 --train_file /home/eunjiwon/Git/Collect-Data-with-BugPatchCollector/TrainData/Master_AllCommitsAddedLines.txt
 
 
 
